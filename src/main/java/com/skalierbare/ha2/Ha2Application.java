552c1f8a68c270bf9cdbdcd9517304bcb0edad38
shatoboar <baesangwoo99@gmail.com>
2021-05-04 19:51:54 +0200

    java backend

---
+++ src/main/java/com/skalierbare/ha2/Ha2Application.java
@@ -0,0 +1,13 @@
+package com.skalierbare.ha2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ha2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ha2Application.class, args);
+	}
+
+}
